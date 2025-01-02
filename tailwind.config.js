83b5cb5142cc48e49ef1ce12309a1ba0bf40aba3
Lucas Craveiro Paes <lucascraveiropaes@gmail.com>
2025-01-02 18:55:14 -0300

    Initial files

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	prefix: 'tw-',
+	content: [
+    "./index.html",
+		"**/*.{html, jsx, js}",
+		"**/*.js",
+		"**/*.html",
+    "!./node_modules/**/*"
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+        main: "#121212"
+      },
+			fontFamily: {
+				poly: ['"poly"', "serif"],
+			},
+		},
+	},
+	plugins: [
+		function({ addVariant }) {
+			addVariant('firefox', ':-moz-any(&)')
+		}
+	],
+}
+
